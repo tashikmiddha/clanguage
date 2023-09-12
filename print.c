15985e22e009fba1c6dbec009e56eb64b0bcb33b
tashikmiddha <136367718+tashikmiddha@users.noreply.github.com>
2023-09-12 23:54:55 +0530

    Create print.c

---
+++ print.c
@@ -0,0 +1,7 @@
+#include<stdio.h>  // This is a preprocessing command and stdio.h is a header file 
+
+int main()  // this is a function with return type integer
+{
+    printf("Hello , world "); //this is a predefined function in stdio.h which print the data on the console
+    return 0;  // this means that the program successfully run
+}
