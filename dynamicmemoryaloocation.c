1432d9fea8021176e8c772d17cab7a75f7708492
tashikmiddha <tashikmiddha@gmail.com>
2023-09-18 00:14:52 +0530

    code on dynamic memory allocation

--- dynamicmemoryaloocation.c
+++ dynamicmemoryaloocation.c
@@ -3,7 +3,19 @@
 
 int main()
 {
- //use of malloc
- int* ptr;
- ptr = (int*)
+    int n;
+    printf("enter the size of integer array\n");
+    scanf("%d",&n);
+    //use of malloc
+    int* ptr=(int* )malloc(n*sizeof(int));
+    for(int i=0;i<n;i++)
+    {
+    scanf("%d",&ptr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+    printf("the value at %d element is %d\n",i,ptr[i]);
+    }
+    free(ptr);
+    return 0;
 }
